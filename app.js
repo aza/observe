9ba6b291e1720f9b6fd8afe6bd32ec9194293df6
Aza <azaaza@gmail.com>
2014-04-18 12:08:15 -0700

    Moving to correct port

--- app.js
+++ app.js
@@ -35,6 +35,6 @@ app.get('/', function(req, res){
   res.send('Hi! <a href="auth/provider">Login</a>')
 })
 
-var server = app.listen(3000, function() {
+var server = app.listen(5000, function() {
     console.log('Listening on port %d', server.address().port);
 });
